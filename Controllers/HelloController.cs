c74ac9100d92bc930a9054ec4d5aae8877c2df52
Miadog7Extra <petter.karstensen@gmail.com>
2024-11-26 09:48:03 +0100

    Hello world

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bobs_Racing.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHelloWorld()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
