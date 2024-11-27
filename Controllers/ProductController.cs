092ea8ea48c0e8b53bb8bdd5e6fe46445eea0d18
Miadog7Extra <petter.karstensen@gmail.com>
2024-11-27 09:19:19 +0100

    Enock fix

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Bobs_Racing.Repositories;
 
 namespace Bobs_Racing.Controllers
 {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Bobs_Racing.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
